6d6f54a835cfbd1574e9ca4567dce5fc38efa615
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-03-31 11:05:51 +0200

    🚧 Tooltip on pink div

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+import { Tooltip } from 'reka-ui/namespaced';
+</script>
+
 <template>
-  <div class="bg-pink-100">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit est necessitatibus unde deserunt. Laborum modi,
-    exercitationem alias iure dolores sit.
+  <div class="flex justify-center items-center h-screen">
+    <Tooltip.Provider :delay-duration="0">
+      <Tooltip.Root>
+        <Tooltip.Trigger>
+          <div class="bg-pink-100 min-h-10 max-h-96 w-96" />
+        </Tooltip.Trigger>
+
+        <Tooltip.Portal>
+          <Tooltip.Content class="bg-gray-600 text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, quod.
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   </div>
 </template>
